c8e81eae0bcc0e1ecdf9fa42b4f41a2deff73151
John Carmack <johncarmack@me.com>
2024-04-08 17:51:21 -0700

    Add set_buffer command with color options

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
+mod buffer;
+
 use anyhow::Context as _;
 use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
 use shuttle_runtime::SecretStore;
 use shuttle_serenity::ShuttleSerenity;
 
-struct Data {} // User data, which is stored and accessible in all command invocations
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-
-#[poise::command(slash_command)]
-async fn set_buffer(ctx: Context<'_>) -> Result<(), Error> {
-    // let tunnel_domain = secret_store
-    //     .get("NGROK_TUNNEL_DOMAIN")
-    //     .context("'NGROK_TUNNEL_DOMAIN' was not found")
-    //     .unwrap();
-    // let tunnel_domain = format!("https://{}", tunnel_domain);
-
-    let tunnel_domain = "https://doe-smooth-seemingly.ngrok-free.app";
-
-    ctx.say(tunnel_domain).await?;
-    Ok(())
-}
+pub struct Data {} // User data, which is stored and accessible in all command invocations
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Responds with "world!"
 #[poise::command(slash_command)]
@@ -30,14 +18,18 @@ async fn hello(ctx: Context<'_>) -> Result<(), Error> {
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_runtime::Secrets] secret_store: SecretStore) -> ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
+    let ngrok_tunnel_domain = secret_store
+        .get("NGROK_TUNNEL_DOMAIN")
+        .context("'NGROK_TUNNEL_DOMAIN' was not found")?;
+    std::env::set_var("NGROK_TUNNEL_DOMAIN", ngrok_tunnel_domain);
+
     let discord_token = secret_store
         .get("DISCORD_TOKEN")
         .context("'DISCORD_TOKEN' was not found")?;
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![hello(), set_buffer()],
+            commands: vec![hello(), buffer::set_buffer()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
